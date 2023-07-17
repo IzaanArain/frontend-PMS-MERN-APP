23709583e100d8723819a20f95980bec6aa375b3
IzaanArain <izaansaquib@gmail.com>
2023-07-17 22:10:04 +0500

    vercel deployment

--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -1,81 +1,93 @@
-import React from 'react'
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { addProductAsync } from '../features/Product/ProductSlice';
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addProductAsync } from "../features/Product/ProductSlice";
 
 const AddProduct = () => {
-  const [newProduct,setNewProduct]=useState({
-    name:"",
-    description:"",
-    price:"",
-    image:""
-  })
+  const [newProduct, setNewProduct] = useState({
+    name: "",
+    description: "",
+    price: "",
+    image: "",
+  });
 
-  const dispatch=useDispatch()
+  const dispatch = useDispatch();
 
-  const AddFormOnChange=(e)=>{
+  const AddFormOnChange = (e) => {
     // console.log(e.target)
-    const {name,value}=e.target
-    setNewProduct({...newProduct,[name]:value})
-  }
+    const { name, value } = e.target;
+    setNewProduct({ ...newProduct, [name]: value });
+  };
   // console.log(newProduct)
 
-  const submitAddForm=(e)=>{
+  const submitAddForm = (e) => {
     e.preventDefault();
-    dispatch(addProductAsync({
-      name:newProduct.name,
-      description:newProduct.description,
-      price:newProduct.price,
-      image:newProduct.image
-    }))
-  }
+    dispatch(
+      addProductAsync({
+        name: newProduct.name,
+        description: newProduct.description,
+        price: newProduct.price,
+        image: newProduct.image,
+      })
+    );
+    setNewProduct({
+      name: "",
+      description: "",
+      price: "",
+      image: "",
+    });
+  };
   return (
     <>
-    <div className='Add_Product_Form'>
+      <div className="Add_Product_Form">
+        <div className="card text-light" id="add_form">
+          <div className="card-body">
+            <Form onSubmit={submitAddForm}>
+              <Form.Group className="mb-3">
+                <Form.Label htmlFor="name">Name : </Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Name"
+                  id="name"
+                  name="name"
+                  value={newProduct.name}
+                  onChange={AddFormOnChange}
+                  required
+                />
+              </Form.Group>
 
-   <div className="card text-light" id='add_form'>
-    <div className="card-body">
-    <Form onSubmit={submitAddForm}>
-      <Form.Group className="mb-3">
-        <Form.Label htmlFor="name">Name : </Form.Label>
-        <Form.Control 
-        type="text" 
-        placeholder="Enter Name" 
-        id='name'
-        name="name"
-        value={newProduct.name}
-        onChange={AddFormOnChange}
-        required/>
-      </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label htmlFor="description">
+                  Description: limit to 100 characters
+                </Form.Label>
+                <Form.Control
+                  as="textarea"
+                  rows={5}
+                  id="description"
+                  name="description"
+                  value={newProduct.description}
+                  onChange={AddFormOnChange}
+                  maxLength={100}
+                  required
+                />
+              </Form.Group>
 
-      <Form.Group className="mb-3">
-        <Form.Label htmlFor="description">Description: limit to 100 characters</Form.Label>
-        <Form.Control 
-        as="textarea"
-        rows={5} 
-        id="description"
-        name="description"
-        value={newProduct.description}
-        onChange={AddFormOnChange}
-        maxLength={100}
-        required/>
-      </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label htmlFor="price">Price : </Form.Label>
+                <Form.Control
+                  type="number"
+                  placeholder="Enter Name"
+                  id="price"
+                  name="price"
+                  value={newProduct.price}
+                  onChange={AddFormOnChange}
+                  required
+                />
+              </Form.Group>
 
-      <Form.Group className="mb-3">
-        <Form.Label htmlFor="price">Price : </Form.Label>
-        <Form.Control 
-        type="number" 
-        placeholder="Enter Name" 
-        id='price'
-        name="price"
-        value={newProduct.price}
-        onChange={AddFormOnChange}
-        required/>
-      </Form.Group>
-
-      {/* <Form.Group className="mb-3">
+              {/* <Form.Group className="mb-3">
         <Form.Label htmlFor="image">image : </Form.Label>
         <Form.Control 
         type="url" 
@@ -86,15 +98,15 @@ const AddProduct = () => {
         onChange={AddFormOnChange}/>
       </Form.Group> */}
 
-      <Button variant="primary" type="submit" style={{width:"100%"}}>
-        Submit
-      </Button>
-    </Form>
-    </div>
-   </div>
-    </div>
+              <Button variant="primary" type="submit" style={{ width: "100%" }}>
+                Submit
+              </Button>
+            </Form>
+          </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default AddProduct
+export default AddProduct;
