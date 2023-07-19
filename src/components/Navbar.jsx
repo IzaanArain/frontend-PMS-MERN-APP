cc57e87943691e1220ec2fad790c2766410a8485
IzaanArain <izaansaquib@gmail.com>
2023-07-19 22:05:10 +0500

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Navbar = () => {
+  const {products}=useSelector((state)=>state.products)
   return (
     <>
     <div className='Navbar'> 
         <h1>Product Management system</h1>
+        <h1>Products ({products.length})</h1>
         <h4>izaansaquib@gmail.com</h4>
     </div>
     </>
